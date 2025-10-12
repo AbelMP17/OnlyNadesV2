2f8044db92405600aec7e42eb4badd1a9cf3b14d
AbelMP17 <abelmp890@gmail.com>
2025-10-12 21:20:46 +0200

    big graphic changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { AuthProvider } from "@/lib/auth";
 import Header from "@/components/Header";
-import { ThemeProvider } from "@/context/ThemeContext"; // <-- nuevo
+import { ThemeProvider } from "@/context/ThemeContext";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "OnlyNades",
@@ -16,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="cs-page min-h-screen">
         <ThemeProvider>
           <AuthProvider>
+            <Analytics />
             <Header />
             <main className="max-w-6xl mx-auto px-4 py-6">{children}</main>
           </AuthProvider>
